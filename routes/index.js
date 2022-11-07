976c99a458964a86c5819da351b4552515675021
Saisha Kashyap <saishakashyap8@gmail.com>
2022-11-07 16:08:25 +0530

    Add getAll

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,33 @@ router.get("/bikes/team", (request, response, next) => {
   response.send(bikes.team());
 });
 
+router.get('/bikes/all', (request, response, next) => {
+
+  let get_params = url.parse(request.url, true).query;
+  if (Object.keys(get_params).length == 0) {
+    response.setHeader('content-type', 'application/json');
+    response.end(JSON.stringify(bikes.list()));
+  } else {
+    let key = Object.keys(get_params)[0]; // get first parameter only
+    let value = request.query[key];
+    try {
+      let result = bikes.query_by_arg(key, value);
+    } catch (err) {
+      console.log("Caught exception.");
+      //createError(500);
+      res.status(500).send('Something broke!');
+    }
+    let result = bikes.query_by_arg(key, value);
+    if (result) {
+        console.log(result);
+      response.setHeader('content-type', 'application/json');
+      response.end(JSON.stringify(result));
+    } else {
+      next(createError(404));
+    }
+  }
+});
+
 router.get("/bikes/all/:location", (request, response, next) => {
   const param = request.params.location;
   console.log("got into bikes/:location " + param);
